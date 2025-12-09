9623f087883b183bda5c28ec2a47b99201abb38f
hibahkhan2022-rgb <hibahkhan2022@gmail.com>
2025-12-08 19:42:43 -0500

    Update and rename livepeople to livepeople.py

---
+++ livepeople.py
@@ -0,0 +1,17 @@
+from jetson_inference import detectNet
+from jetson_utils import videoSource, videoOutput
+
+net = detectNet("ssd-mobilenet-v2", threshold=0.5)
+camera = videoSource("csi://0")      # '/dev/video0' for V4L2
+display = videoOutput("display://0") # 'my_video.mp4' for file
+
+while display.IsStreaming():
+    img = camera.Capture()
+
+    if img is None: # capture timeout
+        continue
+
+    detections = net.Detect(img)
+    
+    display.Render(img)
+    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
